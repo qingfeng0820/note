ef9e29e0df8fb4f55c7a8cef2373d2ca5b432957
Sherman <qingfeng0820@sina.com>
2017-08-25 16:31:09 +0800

    Update and rename README.md to refresh.sql

--- refresh.sql
+++ refresh.sql
@@ -1,24 +1,15 @@
-# note
-
-
-PostgreSQL 9.3 materialized view concurrently refresh
+# PostgreSQL 9.3 materialized view concurrently refresh
 
 CREATE TABLE visitors_copy AS (select * from visitors);
 
 BEGIN;
-
-  ALTER MATERIALIZED VIEW visitors RENAME TO visitors_refreshing;
-  
+  ALTER MATERIALIZED VIEW visitors RENAME TO visitors_refreshing;  
   ALTER TABLE visitors_copy RENAME TO visitors;
-
 COMMIT;
 
 REFRESH MATERIALIZED VIEW visitors_refreshing;
 
 BEGIN;
-
   DROP TABLE visitors;
-  
-  ALTER MATERIALIZED VIEW visitors_refreshing RENAME TO visitors;
-  
+  ALTER MATERIALIZED VIEW visitors_refreshing RENAME TO visitors; 
 COMMIT;
